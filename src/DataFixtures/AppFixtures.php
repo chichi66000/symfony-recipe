782180b91e4d1cb1bd8f74b747d343e6b8315ba2
khanhchi <le.khanhchi@yahoo.fr>
2022-12-31 18:07:00 +0100

    make Etity Mark, relation with User & Recipe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,9 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use Faker\Generator;
+use App\Entity\Mark;
 use App\Entity\User;
+use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
@@ -54,6 +55,7 @@ class AppFixtures extends Fixture
         }
         
         // Recipe
+        $recipes = [];
         for($j = 0; $j < 25; $j++) {
             $recipe = new Recipe();
             $recipe
@@ -71,10 +73,20 @@ class AppFixtures extends Fixture
             for ($k = 0; $k < mt_rand(5, 15); $k++) {
                 $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
             }
+            $recipes[] = $recipe;
             $manager->persist($recipe);
         }
 
-        
+        // Marks
+        foreach($recipes as $recipe) {
+            for($i=0; $i<mt_rand(0, 4); $i++) {
+                $mark = new Mark();
+                $mark->setMark(mt_rand(0, 5))
+                    ->setUser($users[mt_rand(0, count($users) - 1)])
+                    ->setRecipe($recipe);
+                $manager->persist($mark);
+            }
+        }
         $manager->flush();
     }
 }
